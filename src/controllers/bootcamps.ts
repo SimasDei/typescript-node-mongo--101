7097fe4232fc0325c119ac0ddf30b51ec01557bb
Simas <simas@simas-dei.com>
2020-04-07 19:11:53 +0300

    add http status enum, begin creating bootcamp routes

--- src/controllers/bootcamps.ts
+++ src/controllers/bootcamps.ts
@@ -1,9 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 
+import { HTTPStatus } from '../types/HTTPStatus';
+import Bootcamp from '../models/Bootcamp';
+
 export const getBootcamps = (req: Request, res: Response, next: NextFunction) => {
-	res.status(200).json({ success: true, msg: 'Get all bootcamps' });
+	res.status(HTTPStatus.OK).json({ success: true, msg: 'Get all bootcamps' });
 };
 
 export const getById = (req: Request, res: Response, next: NextFunction) => {
-	res.status(200).json({ success: true, msg: `Get bootcamp with id of ${req.params.bootcampId}` });
+	res.status(HTTPStatus.OK).json({ success: true, msg: `Get bootcamp with id of ${req.params.bootcampId}` });
+};
+
+export const createBootcamp = async (req: Request, res: Response, next: NextFunction) => {
+	console.log(req.body);
+
+	res.status(HTTPStatus.CREATED).json({ success: true, msg: 'Get all bootcamps' });
 };
--- src/routes/bootcamps.ts
+++ src/routes/bootcamps.ts
@@ -4,6 +4,6 @@ import * as controller from '../controllers/bootcamps';
 
 export const router = express.Router();
 
-router.route('/').get(controller.getBootcamps);
+router.route('/').get(controller.getBootcamps).post(controller.createBootcamp);
 
 router.route('/:bootcampId').get(controller.getById);
