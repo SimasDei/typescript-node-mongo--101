21b19511bcb9c443787d72deed766e5d42b34f1d
Simas <simas@simas-dei.com>
2020-04-15 16:47:34 +0300

    use course router as resource inside bootcamp router

--- src/routes/bootcamps.ts
+++ src/routes/bootcamps.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import * as controller from '../controllers/bootcamps';
+import { router as courseRouter } from './courses';
 
 export const router = express.Router();
 
@@ -9,3 +10,5 @@ router.route('/').get(controller.getBootcamps).post(controller.createBootcamp);
 router.route('/:bootcampId').get(controller.getById).patch(controller.updateBootcamp).delete(controller.deleteBootcamp);
 
 router.route('/radius/:zipcode/:distance').get(controller.getBootcampsWithinRadius);
+
+router.use('/:bootcampId/courses', courseRouter);
--- src/routes/courses.ts
+++ src/routes/courses.ts
@@ -2,7 +2,6 @@ import express from 'express';
 
 import * as controller from '../controllers/courses';
 
-export const router = express.Router();
+export const router = express.Router({ mergeParams: true });
 
 router.route('/').get(controller.getCourses);
-router.route('/:bootcampId').get(controller.getCourses);
